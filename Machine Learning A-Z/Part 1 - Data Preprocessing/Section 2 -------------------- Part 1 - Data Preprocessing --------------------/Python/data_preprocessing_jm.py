31f655cc8d20292dddf4ff7492f6d7a7b4022c2f
Joanna Moraza <Joanna.Moraza@gmail.com>
2021-01-13 22:26:38 -0800

    Created matrix of features & dependent variable vector. Replaced missing values w/ data mean.

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_jm.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_jm.py
@@ -7,18 +7,45 @@ Created on Mon Dec 28 20:50:01 2020
 """
 
 #Importing libraries
+
 import numpy as np  
-#Imports numpy which allows for working with arrays
+##Imported numpy library which supports array [matrices and vectors] operation.
+
 import pandas as pd 
-#Imports pandas, library that allows working w/ matrix & dataframes
+##Imported pandas library which supports cleaning and preperation of data.
+
 import matplotlib.pyplot as plt  
-#Imports matplot library which allows working w/ graphs & visuals
+##Imported pyplot module from matplot library which supports graphs & visuals.
+
 
 #Importing the dataset
+
+##Created the variable containing the data in dataframe form.
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:,:3].values  #iloc.[:,:-1] is another way of indexing
-y = dataset.iloc[:,3]  #iloc.[:,-1] is another way of indexing
+
+##Creating a matrix of features x (also known as the independent variables). 
+##and a dependent variable vector y (the information you are trying to predict w/ data).
+x = dataset.iloc[:,:3].values
+y = dataset.iloc[:,-1].values
+##.values in panda removes axes titles of dataframe and returns values in an array (like Numpy).
 
 print(x)
 print(y)
+
+#Taking care of missing data
+  ##Missing data could cause errors when training your model.
+  ##Option, remove incomplete data if sample data is large.
+  ##Alternate option, use average to fill in missing data.
+
+##Import scikit learn library which supports data processing and model building tools.
+from sklearn.impute import SimpleImputer  
+
+##Creating an instance/object from the class SimpleImputer.
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+##Using .fit method to look for missing values and compute the mean.
+imputer.fit(x[:,1:3])
+##Using .transform method to replace the missing values w/ the computed mean.
+x[:,1:3] = imputer.transform(x[:,1:3])
+##This returns original matrix x with the new averaged values populated in age & salary column.
+print(x)
 
